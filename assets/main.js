570c8d8488e1835847574a36cf28cd9723afadd0
jusnick <105069401+jusnick@users.noreply.github.com>
2022-06-22 08:34:05 +0300

    Update main.js

---
+++ assets/main.js
@@ -0,0 +1,4 @@
+function displayGreeting() {
+    let usrname = document.getElementById('usrname').value;
+    document.getElementById('outputDiv').innerHTML = 'Hello ' + usrname +  'welcome to the bootcamp';
+  }
